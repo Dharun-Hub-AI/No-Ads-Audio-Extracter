3806fe6f77c55db89c4f86a6c79195719d218a06
Dharun-Hub-AI <dharunprofessional@gmail.com>
2026-06-14 21:10:30 +0530

    Add Render deployment config

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ function runYtDlp(url, args) {
 }
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
